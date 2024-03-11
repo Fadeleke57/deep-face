64cb09fed2b4e69b9e5acc61b86927214c5f21c6
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-03-10 20:04:10 -0400

    Update to comments in main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import threading #parallel processing 
+import threading
 import cv2 as cv
-from deepface import DeepFace #https://github.com/serengil/deepface *might need to downgrade tensorflow to 2.12 bc LocallyConnected2D layer is no longer available in TensorFlow 2.x Keras API. https://stackoverflow.com/questions/78131429/importerror-cannot-import-name-locallyconnected2d-from-tensorflow-keras-laye/78131539#78131539
+from deepface import DeepFace # https://github.com/serengil/deepface
 
       
-cap = cv.VideoCapture(0, cv.CAP_DSHOW) #open cv video capture object (0 refernces the first - and only camera - on my laptop, CAP_DSHOW specifies the video source for windows) 
+cap = cv.VideoCapture(0, cv.CAP_DSHOW) #(0 refernces the first - and only camera - on my laptop, CAP_DSHOW specifies the video source for windows) 
 
 cap.set(cv.CAP_PROP_FRAME_WIDTH, 640) #width and height of displayed video fram
 cap.set(cv.CAP_PROP_FRAME_HEIGHT, 280)
@@ -23,15 +23,17 @@ def check_face(frame):
         face_match = False
 
 while True:
-    can_read, next_frame = cap.read() # cap.read() returns boolean, then the next frame of video 
+    can_read, next_frame = cap.read() # cap.read() returns a boolean and the next frame of video 
 
     if can_read:
         if timer % 25 == 0:
+            pass
+            '''
             try:
                 threading.Thread(target=check_face, args=(next_frame.copy(),)).start() #copy of the frame is passed unto check_face to 
             except ValueError: #deepface returns a value error when there isn't a match (strange)
                 pass
-
+'''
         timer += 1
 
         if face_match:
@@ -39,7 +41,7 @@ while True:
         else:
             cv.putText(next_frame, "NOT VERIFIED", (20, 450), cv.FONT_ITALIC, 2, (0, 0, 255), 3)
 
-        cv.imshow("video frame", next_frame) #displays video stream
+        cv.imshow("video frame", next_frame) # displays video stream
 
 
     key = cv.waitKey(1) #quit when q is pressed
